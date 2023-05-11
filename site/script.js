3582e9960719f4e62d5c4ee823fd9c54b1ef7e6b
Lucas Silva <lucassilmor0@hotmail.com>
2023-05-10 23:41:32 -0300

    animacao hamburguer e alteracoes das cores do site

--- site/script.js
+++ site/script.js
@@ -1,5 +1,5 @@
 let hamburguer = document.querySelector(".hamburguer");
 
 hamburguer.addEventListener("click", function(){
-    document.querySelector(".sidebar").classList.toggle("show-menu")
+    document.querySelector(".container").classList.toggle("show-menu")
 })
