572874354fca623229284a3767ccead2043da06d
Lucas Silva <lucassilmor0@hotmail.com>
2023-05-31 23:33:53 -0300

    alteracao sobre mim e remocao do hamburguer

--- site/script.js
+++ site/script.js
@@ -1,13 +1,12 @@
-let hamburguer = document.querySelector(".hamburguer");
-
-hamburguer.addEventListener("click", function(){
-    document.querySelector(".container").classList.toggle("show-menu")
-})
-
-
 let expandir = document.querySelector(".container-botao");
 
 expandir.addEventListener("click", function(){
     document.querySelector(".grid-container-hidden").classList.toggle("show")
 })
+
+// let hamburguer = document.querySelector(".hamburguer");
+
+// hamburguer.addEventListener("click", function(){
+//     document.querySelector(".container").classList.toggle("show-menu")
+// })
 
